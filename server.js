21cbb0f0dd4571b79982de60f192a2f1f125fee4
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-01-14 08:24:10 -0700

    Added Express and Handlebars, setup middleware, routes, and listener

---
+++ server.js
@@ -0,0 +1,27 @@
+// Set Express
+var express = require("express");
+var app = express();
+
+// Set Port for hosted or local environments
+var PORT = process.env.PORT || 8080;
+
+// Setup middleware so it can parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+// Sets the routes js to use in our Express app
+app.use(routes);
+
+// Start our server so that it can begin listening for client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
